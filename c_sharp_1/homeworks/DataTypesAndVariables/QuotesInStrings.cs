edad646e59544c290bb84c01d5258a4b16b5d1f7
Васил <v.gutev36@gmail.com>
2016-10-15 21:18:33 +0300

    Added QuotesInStrings.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/QuotesInStrings.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace QuotesInStrings
+{
+    class QuotesInStrings
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The \"use\" of quotations causes difficulties.");
+        }
+    }
+}
