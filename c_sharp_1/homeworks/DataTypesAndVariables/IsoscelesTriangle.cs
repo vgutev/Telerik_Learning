bf4e5dd700807b9aa5825eaae532257a0cb8b799
Васил <v.gutev36@gmail.com>
2016-10-15 21:14:10 +0300

    Added IsoscelesTriangle.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/IsoscelesTriangle.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace IsoscelesTriangle
+{
+    class IsoscelesTriangle
+    {
+        static void Main(string[] args)
+        {
+            string a = "   \u00a9 \n  \u00a9 \u00a9 \n \u00a9   \u00a9 \n\u00a9 \u00a9 \u00a9 \u00a9";
+            Console.WriteLine(a);
+        }
+    }
+}
