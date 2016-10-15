ec43b58ebb418f0e71c7e86a22f3c21ca0da3427
Васил <v.gutev36@gmail.com>
2016-10-15 21:11:30 +0300

    Added DeclareVariables.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/DeclareVariables.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DeclareVariables
+{
+    class DeclareVariables
+    {
+        static void Main(string[] args)
+        {
+            ushort a = 52130;
+            sbyte b = -115;
+            int c = 4825932;
+            byte d = 97;
+            short e = -10000;
+        }
+    }
+}
