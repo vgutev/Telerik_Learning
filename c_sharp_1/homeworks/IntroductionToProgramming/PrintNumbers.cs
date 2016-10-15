565b3aa742c933e681260068c54efc28abac4a93
Васил <v.gutev36@gmail.com>
2016-10-15 21:03:25 +0300

    Added PrintNumbers.cs

---
+++ c_sharp_1/homeworks/IntroductionToProgramming/PrintNumbers.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace PrintNumbers
+{
+    class PrintNumbers
+    {
+        static void Main()
+        {
+            Console.Write("1\n101\n1001\n");
+        }
+    }
+}
