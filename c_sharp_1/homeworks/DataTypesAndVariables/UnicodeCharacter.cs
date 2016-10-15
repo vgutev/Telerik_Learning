8b0273f08c74d4d5c6b00cbd7c672eaa6cd4ed6e
Васил <v.gutev36@gmail.com>
2016-10-15 21:19:38 +0300

    Added UnicodeCharacter.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/UnicodeCharacter.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace UnicodeCharacter
+{
+    class UnicodeCharacter
+    {
+        static void Main(string[] args)
+        {
+            char a = '\u002A';
+            Console.WriteLine(a);
+        }
+    }
+}
