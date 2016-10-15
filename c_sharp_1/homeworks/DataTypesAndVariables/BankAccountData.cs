72a850b32d265f6cb2792098fd1fd8ea2e6fdff5
Васил <v.gutev36@gmail.com>
2016-10-15 21:06:08 +0300

    Added BankAccountData.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/BankAccountData.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace BankAccountData
+{
+    class BankAccountData
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Darth";
+            string mName = "Ivanov";
+            string lName = "Vader";
+            decimal balance = 10273.25M;
+            string bankName = "Post Bank";
+            string IBAN = "BG80 BNBG 9661 1020 3456 78";
+            ulong cCard1 = 4805437841899720;
+            ulong cCard2 = 4539203383244383;
+            ulong cCard3 = 4024007121356215;
+        }
+    }
+}
