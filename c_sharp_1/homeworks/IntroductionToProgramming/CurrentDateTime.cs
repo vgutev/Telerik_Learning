b4f4f68a73ca1ce65a57cc2f679ee53ff30f0824
Васил <v.gutev36@gmail.com>
2016-10-15 20:58:31 +0300

    Added CurrentDateTime.cs

---
+++ c_sharp_1/homeworks/IntroductionToProgramming/CurrentDateTime.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CurrentDateTime
+{
+    class CurrentDateTime
+    {
+        static void Main()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
