ec41430caf0537651004297d2e773f459fbcdfb0
Васил <v.gutev36@gmail.com>
2016-10-15 21:12:17 +0300

    Added EmployeeData.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/EmployeeData.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace EmployeeData
+{
+    class EmployeeData
+    {
+        static void Main(string[] args)
+        {
+            string fName = "Kolio";
+            string lName = "Petrov";
+            byte age = 26;
+            char gender = 'm';
+            long pid = 8306112507;
+            uint uiNumber = 27560000;
+            System.Console.WriteLine("{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n", fName, lName, age, gender, pid, uiNumber);
+
+        }
+    }
+}
