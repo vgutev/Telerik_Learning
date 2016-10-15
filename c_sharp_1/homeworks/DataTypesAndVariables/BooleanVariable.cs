0d7be9578a2c2d26538817b66a57067505d81932
Васил <v.gutev36@gmail.com>
2016-10-15 21:10:07 +0300

    Added BooleanVariable.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/BooleanVariable.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace BooleanVariable
+{
+    class BooleanVariable
+    {
+        static void Main(string[] args)
+        {
+            bool isFemale = false;
+            Console.WriteLine(isFemale);
+        }
+    }
+}
