999db7917d48638fc290b7ad700d17332b873ac5
Васил <v.gutev36@gmail.com>
2016-10-15 21:04:44 +0300

    Added SquareRoot.cs

---
+++ c_sharp_1/homeworks/IntroductionToProgramming/SquareRoot.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace SquareRoot
+{
+    class SquareRoot
+    {
+        static void Main()
+        {
+            double input=12345;
+            Console.WriteLine(Math.Sqrt(input)); 
+        }
+    }
+}
