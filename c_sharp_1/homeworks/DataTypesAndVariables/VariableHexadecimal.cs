8cedc812aa37ebf136423942b55b5a0ca5a47d93
Васил <v.gutev36@gmail.com>
2016-10-15 21:20:13 +0300

    Added VariableHexadecimal.cs

---
+++ c_sharp_1/homeworks/DataTypesAndVariables/VariableHexadecimal.cs
@@ -0,0 +1,14 @@
+using System;
+
+
+namespace VariableHexadecimal
+{
+    class VariableHexadecimal
+    {
+        static void Main(string[] args)
+        {
+            int a = 0xFE;
+            Console.WriteLine(a);
+        }
+    }
+}
