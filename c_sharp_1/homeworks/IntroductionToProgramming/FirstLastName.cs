4a4ff23fffd4d926d04aec0207aa7e11710d9949
Васил <v.gutev36@gmail.com>
2016-10-15 21:00:04 +0300

    Added FirstLastName.cs

---
+++ c_sharp_1/homeworks/IntroductionToProgramming/FirstLastName.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FirstLastName
+{
+    class FirstLastName
+    {
+        static void Main()
+        {
+            Console.Write("Kalin\nStefanov\n");
+        }
+    }
+}
