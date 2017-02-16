593dd17d2f0ac2227b15f8011c7579f9cda2a84f
vgutev <v.gutev36@gmail.com>
2017-02-16 11:27:04 +0200

    Unit testing workshop update

---
+++ c_sharp_unit_testing/Workshop/Academy.Tests/Commands/Adding/AddStudentToSeasonCommandTests/Constructor_Should.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Academy.Commands.Adding;
+using NUnit.Framework;
+using Moq;
+using Academy.Core.Contracts;
+
+namespace Academy.Tests.Commands.Adding.AddStudentToSeasonCommandTests
+{
+    [TestFixture]
+    class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentException_WhenPassedProviderIsNull()
+        {
+            // Arrange
+            var engineMock = new Mock<IEngine>();
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new AddStudentToSeasonCommand(null, engineMock.Object));
+        }
+    }
+}
---
+++ c_sharp_unit_testing/Workshop/Academy.Tests/Core/Factories/AcademyFactoryTests/CreateLectureResource_Should.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Academy.Core.Factories;
+using Moq;
+using Academy.Core.Contracts;
+
+namespace Academy.Tests.Core.Factories.AcademyFactoryTests
+{
+    [TestFixture]
+    class CreateLectureResource_Should
+    {
+        [Test]
+        public void ThrowArgumenException_WhenPassedTypeIsInvalid()
+        {
+            //arrange
+            var academyFactoryUnderTest = AcademyFactory.Instance;
+            //act and assert
+            Assert.Throws<ArgumentException>(() => academyFactoryUnderTest.CreateLectureResource("banan", "Banan", "htt:\\abv.bg"));
+        }
+    }
+}
